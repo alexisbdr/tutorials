0c359c56df039422a9a9700cdb321db94247f5d5
Alexis Baudron <alexis.baudron15@gmail.com>
2023-12-18 15:09:35 -0800

    fundamentals

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+##### READING AN IMAGE ##########
+img_name = "starry_night.jpg"
+
+
+##### DECOMPOSING IMAGE COLORS ######
+
+
+
+##### THRESHOLDING AN IMAGE ######
+
+
+
+##### MORPHOLOGICAL OPERATORS ######
+
+
+
+##### 
